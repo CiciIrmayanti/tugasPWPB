aca3257706a47af6239a5aed1ff968ccfd5c29c0
CiciIrmayanti <55769697+CiciIrmayanti@users.noreply.github.com>
2020-04-08 13:12:48 +0700

    menambahkan nomor hp dan email

--- aboutme.php
+++ aboutme.php
@@ -1,6 +1,7 @@
 <?php
-echo "Nama  : Cici Irma Yanti<br>";
-echo "Kelas : XI RPL 2<br>";
-echo "Asal Sekolah : SMK TARUNA BHAKTI<br>";
-echo "Alamat : Kp. Bojong Lio RT.06/ RW.28";
+echo "Nama  : Cici Irma Yanti <br>";
+echo "Kelas : XI RPL 2 <br>";
+echo "Asal Sekolah : SMK TARUNA BHAKTI <br>";
+echo "Nomor HP : 085777848928 <br>";
+echo "Email : ciciirmayanti.belgi15@gmail.com";
 ?>
